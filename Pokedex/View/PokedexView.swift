4404e6c449a07a0c9eacd961dbd9a9c08281f2de
dominikdeimel <info@dominikdeimel.com>
2022-06-11 16:00:35 +0200

    chore: UI Skeleton

---
+++ Pokedex/View/PokedexView.swift
@@ -0,0 +1,30 @@
+//
+//  PokedexView.swift
+//  Pokedex
+//
+//  Created by Dominik Deimel on 11.06.22.
+//
+
+import SwiftUI
+
+struct PokedexView: View {
+    private let gridItems = [GridItem(.flexible()), GridItem(.flexible())]
+    
+    var body: some View {
+        NavigationView{
+            ScrollView {
+                LazyVGrid(columns: gridItems, spacing:  16) {
+                    ForEach(0..<151) { _ in
+                        PokemonCell()
+                    }
+                }
+            }
+        }
+    }
+}
+
+struct PokedexView_Previews: PreviewProvider {
+    static var previews: some View {
+        PokedexView()
+    }
+}
